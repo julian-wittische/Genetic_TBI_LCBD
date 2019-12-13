8569e8c3174e77c67245d77199328ec5efc45a90
Julian WITTISCHE <jwittische@gmail.com>
2019-12-12 19:36:19 -0500

    fffffffffff

--- Fig2.R
+++ Fig2.R
@@ -64,3 +64,63 @@ CM <- load_RData("C_M_m.RData")
 CH <- load_RData("C_H_m.RData")
 
 alpha <- c(0.001, 0.0025, 0.005, 0.0075, 0.01, 0.025, 0.05, 0.075, 0.1)
+
+dfFPR_I <- data.frame(alpha,
+                      IL1=sum_perf(load_perf(IL1, 1), "FPR"),
+                      IL2=sum_perf(load_perf(IL2, 2), "FPR"),
+                      IL3=sum_perf(load_perf(IL3, 3), "FPR"),
+                      IM1=sum_perf(load_perf(IM1, 1), "FPR"),
+                      IM2=sum_perf(load_perf(IM2, 2), "FPR"),
+                      IM3=sum_perf(load_perf(IM3, 3), "FPR"),
+                      IH1=sum_perf(load_perf(IH1, 1), "FPR"),
+                      IH2=sum_perf(load_perf(IH2, 2), "FPR"),
+                      IH3=sum_perf(load_perf(IH3, 3), "FPR"))
+
+dfFNR_I <- data.frame(alpha,
+                      IL1=sum_perf(load_perf(IL1, 1), "FNR"),
+                      IL2=sum_perf(load_perf(IL2, 2), "FNR"),
+                      IL3=sum_perf(load_perf(IL3, 3), "FNR"),
+                      IM1=sum_perf(load_perf(IM1, 1), "FNR"),
+                      IM2=sum_perf(load_perf(IM2, 2), "FNR"),
+                      IM3=sum_perf(load_perf(IM3, 3), "FNR"),
+                      IH1=sum_perf(load_perf(IH1, 1), "FNR"),
+                      IH2=sum_perf(load_perf(IH2, 2), "FNR"),
+                      IH3=sum_perf(load_perf(IH3, 3), "FNR"))
+
+dfcontrol <- data.frame(alpha,
+                        CL=sum_perf(load_perf(CL, "control"), "FPR", TRUE),
+                        CM=sum_perf(load_perf(CM, "control"), "FPR", TRUE),
+                        CH=sum_perf(load_perf(CH, "control"), "FPR", TRUE))
+
+sz <- 3
+sz2 <- 1
+col1 <- "yellow"
+col2 <- "orange"
+col3 <- "red"
+
+dfFNR_melt <- melt(dfFNR ,  id.vars = 'alpha', variable.name = 'scenario')
+dfFNR_melt_cb <- cbind(dfFNR_melt, disp=rep("L","M","H", each = 27), pops=rep)
+
+#################################################################################
+
+ggplot(dfFNR_I, aes(alpha, y = value)) + 
+  geom_point(aes(y = IL1),  color = col1, pch=17, size=sz) +
+  geom_point(aes(y = IL2),  color =col2, pch=17, size=sz) +
+  geom_point(aes(y = IL3),  color =col3, pch=17, size=sz) +
+  geom_point(aes(y = IM1),  color =col1, pch=15, size=sz) +
+  geom_point(aes(y = IM2),  color =col2, pch=15, size=sz) +
+  geom_point(aes(y = IM3),  color =col3, pch=15, size=sz) +
+  geom_point(aes(y = IH1),  color =col1, pch=19, size=sz) +
+  geom_point(aes(y = IH2),  color =col2, pch=19, size=sz) +
+  geom_point(aes(y = IH3),  color =col3, pch=19, size=sz) +
+  geom_line(aes(y = IL1),  color =col1, size=sz2) +
+  geom_line(aes(y = IL2),  color =col2, size=sz2) +
+  geom_line(aes(y = IL3),  color =col3, size=sz2) +
+  geom_line(aes(y = IM1),  color =col1, size=sz2) +
+  geom_line(aes(y = IM2),  color =col2, size=sz2) +
+  geom_line(aes(y = IM3),  color =col3, size=sz2) +
+  geom_line(aes(y = IH1),  color =col1, size=sz2) +
+  geom_line(aes(y = IH2),  color =col2, size=sz2) +
+  geom_line(aes(y = IH3),  color =col3, size=sz2) + 
+  labs(fill = "scenario") +
+  theme(text=element_text(size=12,  family="serif"))
